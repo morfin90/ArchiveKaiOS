4c3fd85cb74b7642e968071b3e48dbe539f6d05e
morfin90 <gamer.msh@gmail.com>
2025-07-24 22:46:06 +0330

    Create MenuActivity.java

---
+++ app/src/main/java/com/example/kaioslauncher/MenuActivity.java
@@ -0,0 +1,12 @@
+package com.example.kaioslauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
